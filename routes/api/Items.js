a59f23bfc9876431b03c943acff52e692d7ac0db
jsalonga23 <johnsalonga2@gmail.com>
2018-08-04 13:38:14 +0400

    Delete on the Database

--- routes/api/Items.js
+++ routes/api/Items.js
@@ -13,6 +13,27 @@ router.get('/', (req, res) => {
     .then(items => res.json(items))
 });
 
+// @route Post api/items
+// @desc Create a post
+// @access Public
+router.post('/', (req, res) => {
+  const newItem = new Item({
+    name: req.body.name
+  });
+
+  newItem.save().then(item => res.json(item));
+});
+
+// @route Delete api/items
+// @desc Delete Item
+// @access Public
+router.delete('/:id', (req, res) => {
+  Item.findById(req.params.id)
+    .then(item => item.remove().then(() => res.json({message:'Item Deleted.'})))
+    .catch(err=>res.status(404).json({message : 'Item not found.'}));
+})
+
+
 
 
 module.exports = router;
