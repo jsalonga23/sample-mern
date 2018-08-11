81187363cf8089a0c3b1dde5de64fc02535c2218
jsalonga23 <johnsalonga2@gmail.com>
2018-08-11 12:06:44 +0400

    Creating functions for Showing List and Deleting list

--- client/src/components/ShoppingList.js
+++ client/src/components/ShoppingList.js
@@ -1,33 +1,43 @@
 import React, { Component } from 'react';
-
-import {
-  Container,
-  ListGroup,
-  ListGroupItem,
-  Button
-} from 'reactstrap';
-
-import {
-  CSSTransition,
-  TransitionGroup
-} from 'react-transition-group';
+import {Container,ListGroup,ListGroupItem,Button} from 'reactstrap';
+import {CSSTransition,TransitionGroup} from 'react-transition-group';
 import uuid from 'uuid';
 
 class ShoppingList extends Component{
 
-  state = {
-    items: [
-      {id: uuid(), name: 'Eggs'},
-      {id: uuid(), name: 'Milk'},
-      {id: uuid(), name: 'Steak'},
-      {id: uuid(), name: 'Water'},
-    ]
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: [
+        {id: uuid(), name: 'Eggs'},
+        {id: uuid(), name: 'Milk'},
+        {id: uuid(), name: 'Steak'},
+        {id: uuid(), name: 'Water'},
+      ]
+    }
+
+    this.showList = this.showList.bind(this);
+    this.deleteList = this.deleteList.bind(this);
   }
 
-  addState() {
-
-
+  showList() {
+    const name = prompt('Enter Item');
+    if(name) {
+      this.setState(state => ({
+        items: [...state.items, {id: uuid(), name}]
+      }));
+    }
   }
+
+  deleteList(e) {
+    var id = e.target.value;
+    this.setState(state => ({
+      items: state.items.filter(item => item.id !== id)
+    }));
+  }
+
+
 
   render() {
 
@@ -36,37 +46,14 @@ class ShoppingList extends Component{
     return (
 
       <Container>
-        <Button
-          color="dark"
-          style={{marginBottom: '2rem'}}
-          onClick= {
-            () => {
-              const name = prompt('Enter Item');
-              if(name) {
-                this.setState(state => ({
-                  items: [...state.items, {id: uuid(), name}]
-                }));
-              }
-            }
-          }
-        >Add Item</Button>
+        <Button color="dark" style={{marginBottom: '2rem'}} onClick={this.showList.bind(this)}>Add Item</Button>
 
         <ListGroup>
           <TransitionGroup className="shopping-list">
             {items.map(({id, name}) => (
               <CSSTransition key={id} timeout={500} classNames="fade">
                 <ListGroupItem>
-                  <Button
-                    className="remove-btn"
-                    color="danger"
-                    size="sm"
-                    onClick = {() => {
-                      this.setState(state => ({
-                        items: state.items.filter(item => item.id !== id)
-                      }));
-                    }}
-                  >
-
+                  <Button className="remove-btn" color="danger" size="sm" value={id} onClick={this.deleteList.bind(this)}>
                   &times;</Button>
                   {name}
                 </ListGroupItem>
